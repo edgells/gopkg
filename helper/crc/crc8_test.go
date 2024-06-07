f6b4de0b7f2f542667300756c5a4090ae32063c6
hupei <hupcjp@163.com>
2024-06-07 23:11:49 +0800

    add crc8

--- helper/crc/crc8_test.go
+++ helper/crc/crc8_test.go
@@ -1,8 +1,39 @@
 package crc
 
-import "testing"
+import (
+	"encoding/hex"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func Crc8Bit(data []byte) []byte {
+	var byteData byte
+	for _, val := range data {
+		byteData += val
+	}
+
+	return append(data, byteData)
+}
 
 func TestCrc8(t *testing.T) {
 	data := []byte{0xFA, 0x01, 0xF6, 0x02, 0x80, 0x02}
 	t.Log(crc8(data))
+}
+
+func TestCrc8bit(t *testing.T) {
+	data := []byte{0xFA, 0x01, 0xFD, 0x80, 0xF5, 0xFF, 0x00, 0x00, 0x03, 0x20}
+
+	t.Logf("%x\n", Crc8Bit(data))
+}
+
+func TestCrc8Pos(t *testing.T) {
+	hexString := "FA 01 FD 02 80 02 00 FA 00"
+	hexString = strings.ReplaceAll(hexString, " ", "")
+	data, err := hex.DecodeString(hexString)
+	if err != nil {
+		return
+	}
+
+	t.Log(fmt.Sprintf("%x", Crc8Bit(data)))
 }
